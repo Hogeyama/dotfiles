b7268b3adf9136a32b62eddc3fd8675199ecc11d
Hogeyama <gan13027830@gmail.com>
2015-12-01 11:50:46 +0900

    delete .vim & .nvim

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.nvim ~/.nvim
-ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimperator ~/.vimperator
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
