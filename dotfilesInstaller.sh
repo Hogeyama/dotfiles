4929358cb9dcac9194d33ce62c98803d83f4ded6
Hogeyama <gan13027830@gmail.com>
2016-03-05 22:28:46 +0900

    rm vimperator files & ghc-mod-check.sh

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-ln -s ~/dotfiles/.vimperator      ~/.vimperator
+ln -s ~/dotfiles/.profile         ~/.profile
 ln -s ~/dotfiles/.bashrc          ~/.bashrc
+ln -s ~/dotfiles/.bash_aliases    ~/.bash_aliases
+ln -s ~/dotfiles/.vimrc           ~/.vimrc
 ln -s ~/dotfiles/.gvimrc          ~/.gvimrc
 ln -s ~/dotfiles/.latexmkrc       ~/.latexmkrc
-ln -s ~/dotfiles/.profile         ~/.profile
-ln -s ~/dotfiles/.vimperatorrc    ~/.vimperatorrc
-ln -s ~/dotfiles/.vimrc           ~/.vimrc
 ln -s ~/dotfiles/.latex           ~/.latex
 ln -s ~/dotfiles/.pandoc          ~/.pandoc
 ln -s ~/dotfiles/init.vim         ~/.config/nvim/init.vim
-ln -s ~/dotfiles/ghc-mod-check.sh ~/.local/bin/ghc-mod-check.sh
-
-
--- ghc-mod-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TARGET=`realpath $1`
-ROOT=`ghc-mod root`
-cd $ROOT
-ghc-mod check $TARGET | sed -e "s#^#$ROOT/#" | sed -e 's#\([^:]*:[^:]*:[^:]*:\)#\1\n#' | sed -e "s#\x0#\n#g"
