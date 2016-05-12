1c8c507e24a16e75200bcd29560a7e68c375741f
Hogeyama <gan13027830@yahoo.co.jp>
2016-05-12 20:39:07 +0900

    commentize .vimrc & .gvimrc:)

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -3,8 +3,8 @@ ln -sf ~/dotfiles/.profile         ~/.profile
 ln -sf ~/dotfiles/.bashrc          ~/.bashrc
 ln -sf ~/dotfiles/.bashenv         ~/.bashenv
 ln -sf ~/dotfiles/.bash_aliases    ~/.bash_aliases
-ln -sf ~/dotfiles/.vimrc           ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc          ~/.gvimrc
+#ln -sf ~/dotfiles/.vimrc           ~/.vimrc
+#ln -sf ~/dotfiles/.gvimrc          ~/.gvimrc
 ln -sf ~/dotfiles/.latexmkrc       ~/.latexmkrc
 ln -sf ~/dotfiles/.latex           ~/.latex
 ln -sf ~/dotfiles/.pandoc          ~/.pandoc
