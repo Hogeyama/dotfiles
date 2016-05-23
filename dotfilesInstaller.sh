84f13656023a0b92d87038d1498adb96c52d7b0b
Hogeyama <gan13027830@gmail.com>
2016-05-23 17:39:08 +0900

    add .i3/config

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -10,3 +10,4 @@ ln -sf ~/dotfiles/.latex           ~/.latex
 ln -sf ~/dotfiles/.pandoc          ~/.pandoc
 ln -sf ~/dotfiles/init.vim         ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.Xmodmap         ~/.Xmodmap
+ln -sf ~/dotfiles/.i3/config       ~/.i3/config
