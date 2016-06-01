d848c9ebf749366f6a77961583398fd3ff5a3940
Hogeyama <gan13027830@gmail.com>
2016-06-01 15:50:35 +0900

    mkdir improve: check if dir exist

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-mkdir -p ~/.config/nvim
-mkdir ~/.i3
+if [ ! -d ~/.config/nvim ]; then
+    mkdir -p ~/.config/nvim
+fi
+
+if [ ! -d ~/.i3 ]; then
+    mkdir ~/.i3
+fi
 
 ln -sf ~/dotfiles/.profile         ~/.profile
 ln -sf ~/dotfiles/.bashrc          ~/.bashrc
 ln -sf ~/dotfiles/.bashenv         ~/.bashenv
 ln -sf ~/dotfiles/.bash_aliases    ~/.bash_aliases
-#ln -sf ~/dotfiles/.vimrc           ~/.vimrc
-#ln -sf ~/dotfiles/.gvimrc          ~/.gvimrc
 ln -sf ~/dotfiles/.latexmkrc       ~/.latexmkrc
 ln -sf ~/dotfiles/.latex           ~/.latex
 ln -sf ~/dotfiles/.pandoc          ~/.pandoc
