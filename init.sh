7736b758b068cab758329937d55cc5d01535dce0
Hogeyama <gan13027830@gmail.com>
2016-06-01 13:04:01 +0900

    rename hogeInstaller.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#neovim
+sudo apt-get install git python-dev python3-dev python-pip python3-pip
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
+#stack
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+echo 'deb http://download.fpcomplete.com/ubuntu trusty main'| sudo tee /etc/apt/sources.list.d/fpco.list
+sudo apt-get update && sudo apt-get install stack -y
+
+#opam
+add-apt-repository ppa:avsm/ppa
+apt-get update
+apt-get install ocaml ocaml-native-compilers camlp4-extra opam
+
+
