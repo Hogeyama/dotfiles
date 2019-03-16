d121547a8b6e4878910fcc11899dc4d1ff42d006
Hogeyama <gan13027830@gmail.com>
2019-03-16 19:46:35 +0900

    update

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -11,15 +11,10 @@ ln -s ~/dotfiles/.zshrc                ~/.zshrc
 ln -s ~/dotfiles/.zshenv               ~/.zshenv
 ln -s ~/dotfiles/.zsh_alias            ~/.zsh_alias
 
-#ln -s ~/.config/nvim                   ~/.nvim
 ln -s ~/dotfiles/nvim/init.vim         ~/.config/nvim/init.vim
 ln -s ~/dotfiles/nvim/ginit.vim        ~/.config/nvim/ginit.vim
 ln -s ~/dotfiles/nvim/indent           ~/.config/nvim/indent
 ln -s ~/dotfiles/nvim/snippets         ~/.config/nvim/snippets
-ln -s ~/dotfiles/nvim/my-nvim-hs.cabal ~/.config/nvim/my-nvim-hs.cabal
-ln -s ~/dotfiles/nvim/nvim.hs          ~/.config/nvim/nvim.hs
-ln -s ~/dotfiles/nvim/Sample.hs        ~/.config/nvim/Sample.hs
-ln -s ~/dotfiles/nvim/stack.yaml       ~/.config/nvim/stack.yaml
 
 ln -s ~/dotfiles/.apvlvrc              ~/.apvlvrc
 ln -s ~/dotfiles/.latexmkrc            ~/.latexmkrc
