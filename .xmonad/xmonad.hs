88b519d0cf2ccc4ef276fa82f9c303d7e5a179ef
Hogeyama <gan13027830@gmail.com>
2020-06-01 15:08:41 +0900

    Revert "xmonad: switch from stack to cabal"
    This reverts commit 7541a4747ef391ab2c019fbc18599f83a9d63e85.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,10 +9,12 @@ module Main where
 {- {{{ -}
 import           RIO
 import           System.Process
+import           System.Environment
 import           System.IO.Unsafe               ( unsafePerformIO )
-import           System.IO                      ( hPutStrLn )
+import           System.IO                      ( hPutStrLn, hGetContents )
 
 import           XMonad
+import           XMonad.Config.Prime            ( io )
 import           XMonad.Hooks.EwmhDesktops      ( ewmh )
 import           XMonad.Hooks.DynamicLog        ( PP(..)
                                                 , xmobarPP
@@ -24,10 +26,15 @@ import           XMonad.Hooks.ManageDocks       ( AvoidStruts
                                                 , avoidStruts
                                                 , ToggleStruts(..)
                                                 )
+import           XMonad.Operations              ( kill )
 import qualified XMonad.StackSet                as W
 import           XMonad.Util.EZConfig           ( additionalKeys
                                                 , additionalKeysP
                                                 , removeKeysP
+                                                )
+import           XMonad.Layout                  ( Choose
+                                                , Full(..)
+                                                , (|||)
                                                 )
 import           XMonad.Layout.Decoration       ( Decoration
                                                 , DefaultShrinker
@@ -220,9 +227,12 @@ toggleTouchPad = setTouchPad . not =<< isTouchPadEnabled
 xmobar' :: LayoutClass l Window
         => XConfig l -> IO (XConfig (ModifiedLayout AvoidStruts l))
 xmobar' conf = do
-    -- h <- spawnPipe "(cd $HOME/.xmonad; cabal exec -- xmobar xmobar.hs)"
-    h <- spawnPipe "xmobar $HOME/.xmonad/xmobar.hs"
-    log' "GABA"
+    h <- spawnPipe "(cd $HOME/.xmonad; stack exec -- xmobar xmobar.hs)"
+    -- (h,hout,herr,_) <- runXmobar
+    -- hSetBuffering hout NoBuffering
+    -- hSetBuffering herr NoBuffering
+    -- void $ async $ log' =<< hGetContents hout
+    -- void $ async $ log' =<< hGetContents herr
     return $ docks $ conf
         { layoutHook = avoidStruts (layoutHook conf)
         , logHook = do logHook conf
