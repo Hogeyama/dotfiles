70cfb67d0f386dd3d011eee627218dfe4a9a51b9
Hogeyama <gan13027830@gmail.com>
2021-05-16 03:14:30 +0900

    Minor

--- win-clipboard.sh
+++ win-clipboard.sh
@@ -6,18 +6,18 @@ get_timestamp() {
 chcp.com 65001 > /dev/null
 
 CLIPBOARD=~/.clipboard
-last=$(get_timestamp $CLIPBOARD)
+last=$(get_timestamp "$CLIPBOARD")
 while true; do
-  sleep $INTERVAL
+  sleep "$INTERVAL"
   echo "last: $last"
   echo "curr: $current"
-  current=$(get_timestamp $CLIPBOARD)
-  if [ $last != $current ] ; then
-    last=$current
+  current=$(get_timestamp "$CLIPBOARD")
+  if [ "$last" != "$current" ] ; then
+    last="$current"
     echo "updated @$current"
-    cat $CLIPBOARD
+    cat "$CLIPBOARD"
     echo "EOF"
     # iconv -c -f utf-8 -t cp932 $CLIPBOARD | clip
-    cat $CLIPBOARD | clip
+    cat "$CLIPBOARD" | clip
   fi
 done
